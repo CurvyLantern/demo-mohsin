306679378991470712acb774aa302669e80bc20f
naseem(T_T) <ashfaqnaseem1@gmail.com>
2023-05-17 17:33:13 +0600

    initial push to repo

---
+++ src/components/BasicComp.js
@@ -0,0 +1,9 @@
+import styles from "./style.module.css";
+
+const BasicComp = () => {
+  return (
+    <div className={styles.ahsdkjahdhahsdj}>this is a basic component</div>
+  );
+};
+
+export default BasicComp;
